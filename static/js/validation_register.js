923217391fb68e6c9023d40609c21bfe7df8b38d
Andrea Carolina <119058364+BergnaCaro@users.noreply.github.com>
2023-11-17 02:23:08 -0300

    Modificaciones a registro y validación y css para formulario de registro
     Changes to be committed:
    	modified:   register.html
    	new file:   static/css/register2.css
    	modified:   static/css/registro.css
    	modified:   static/js/validation_register.js

--- static/js/validation_register.js
+++ static/js/validation_register.js
@@ -1,81 +1,98 @@
-//Funciones
-//Mayoria de edad
-function calculoEdad(fecha){
-    var today = new Date();
-    //Restamos los años
-    años = today.getFullYear() - año;
-    // Si no ha llegado su cumpleaños le restamos el año por cumplir
-    if (fecha.getMonth() > (today.getMonth()) || fecha.getDay() > today.getDay())
-        años--;
-    return años;
+const formulario = document.getElementById('formulario');
+const inputs =document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
-//Email
-function EmailValido(email){
-    
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
-        resp= true;
-       } else {
-        resp= false;
-       }
-    return resp;
+const campos = {
+    usuario: false,
+    nombre: false,
+    password: false,
+    correo: false,
+    telefono: false
 }
-//Validación de datos al registrarse
-//Vincular con register.html
 
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "usuario":
+           ValidarCampo(expresiones.usuario, e.target, 'usuario');
+        break;
 
+        case "nombre":
+            ValidarCampo(expresiones.nombre, e.target, 'nombre');
+        break;
 
-const validacion = (evento) => {
-    evento.preventDefault();
+        case "password":
+            ValidarCampo(expresiones.password, e.target, 'password');
+            ValidarPassword2();
+        break;
 
-    //seleccionando elementos
-    const nombre = document.querySelector('#firstname');
-    const apellido = document.querySelector('#lastname');
-    let validation = true;
+        case "password2":
+            ValidarPassword2();
+        break;
 
-    if(nombre.value===''){
-        validation = false;
-        alert('Por favor, ingrese su nombre')
-    }
-    
-    if(apellido.value===''){
-        validation = false;
-        alert('Por favor, ingrese su apellido');
+        case "correo":
+            ValidarCampo(expresiones.correo, e.target, 'correo');
+        break;
 
-    }
-    if(validation){
-        formRegister.submit();
-    }else{ return false; }
-
-    //validando fecha de nacimiento No me funciona bien !!!
-    const fecha = document.querySelector('#birthday');
-    let valido = true;
-    años = calculoEdad(fecha);
-    if(años.value<18){
-        valido =false;
-        alert('Usted es menor de edad');
+        case "telefono":
+            ValidarCampo(expresiones.telefono, e.target, 'telefono');
+        break;
 
     }
+}
 
-    //validando email con funcion
-    const correo = document.querySelector('#emailname');
-    let validado = true;
-    email =EmailValido(correo);
-    if(email.value===false){
-        validado = false;
-        alert('El correo electrónico ingresado no es válido');
+const ValidarCampo = (expresion, input, campo) =>{
+    if(expresion.test(input.value)){
+        document.getElementById(`grupo_${campo}`).classList.remove('formulario_grupo-incorrecto');
+        document.querySelector(`#grupo_${campo} .formulario_input-error`).classList.remove('formulario_input-error-activo');
+        campos[campo] = true;
+
+    }else{
+        document.getElementById(`grupo_${campo}`).classList.add('formulario_grupo-incorrecto');
+        document.querySelector(`#grupo_${campo} .formulario_input-error`).classList.add('formulario_input-error-activo');
+        campos[campo] = false;
+    }
+}
+
+const ValidarPassword2 = () =>{
+    const inputPassword1 = document.getElementById('password');
+    const inputPassword2 = document.getElementById('password2');
+
+    if(inputPassword1.value !== inputPassword2.value){
+        document.getElementById(`grupo_password2`).classList.add('formulario_grupo-incorrecto');
+        document.querySelector(`#grupo_password2 .formulario_input-error`).classList.add('formulario_input-error-activo');
+        campos['password'] = false;
+    } else {
+        document.getElementById(`grupo_password2`).classList.remove('formulario_grupo-incorrecto');
+        document.querySelector(`#grupo_password2 .formulario_input-error`).classList.remove('formulario_input-error-activo');
+        campos['password'] = true;
+    }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit' , (e) =>{
+    e.preventDefault();
+
+    const terminos = document.getElementById('terminos');
+
+    if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked){
+        formulario.reset();
+        document.getElementById('formulario_mensaje-exito').classList.add('formulario_mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('formulario_mensaje-exito').classList.remove('formulario_mensaje-exito-activo');
+        }, 5000);
+    }else{
+        document.getElementById('formulario_mensaje').classList.add('formulario_mensaje-activo');
     }
 
-
-
-    //Validar contraseña (en este caso no estoy segura de como hacer)
-   /*
-   Creo que va depender del tipo de caracteres que aceptemos y
-   supongo que dependera de la base de datos para constatar 
-   si ya no esta registrado
-
-   */
-   
-
-};
-
+});
